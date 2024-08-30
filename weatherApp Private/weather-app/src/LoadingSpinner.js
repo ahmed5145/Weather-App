37ae0b21e5df40e0585533483c55ec4e0cc3d5c0
Ahmed Mohamed <68402624+ahmed5145@users.noreply.github.com>
2024-08-30 03:25:28 -0500

    Delete weatherApp Private directory

--- weatherApp Private/weather-app/src/LoadingSpinner.js
+++
@@ -1,9 +0,0 @@
-// src/LoadingSpinner.js
-import React from 'react';
-import './LoadingSpinner.css';
-
-const LoadingSpinner = () => {
-  return <div className="spinner"></div>;
-};
-
-export default LoadingSpinner;
