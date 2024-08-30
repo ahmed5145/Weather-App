6c05582f1b2a866950155a33b272482566389d69
Ahmed Mohamed <ahmed5145>
2024-08-30 03:19:19 -0500

    private for deployment

---
+++ weatherApp Private/weather-app/src/LoadingSpinner.js
@@ -0,0 +1,9 @@
+// src/LoadingSpinner.js
+import React from 'react';
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+  return <div className="spinner"></div>;
+};
+
+export default LoadingSpinner;
